15d75134678bd8e86e143ca2377ee29c7bd9ca8d
ThillaiNandhini <letsmailthillainandhini@gmail.com>
2022-07-10 12:24:17 +0530

    test

---
+++ week1/day1/Mobile.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Mobile {
+  private void makecall() {
+	
+	 System.out.println("make call"); 
+ 
+  }
+  
+  private void sendmsg() {
+	   System.out.println("send Msg");
+  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+System.out.println("This is my Mobile");
+
+	} 
+
+}
